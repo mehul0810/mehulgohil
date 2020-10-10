347ec5858dbcb80b28179442803878cdbe10902a
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-10-10 18:21:15 +0530

    My first npm module

---
+++ README.md
@@ -0,0 +1,5 @@
+## Know More About Me via `npx mehulgohil`
+
+This is a simplest npm module that will help you know more about Mehul Gohil.
+
+Run command: `npx mehulgohil`
---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const log = console.log;
+const chalk = require( 'chalk' );
+
+log(
+    `
+    ${chalk.bold(`------------------------------------------`)}
+    ${chalk.bold(`| Mehul Gohil - WordPress Developer |`)}
+    ${chalk.bold(`------------------------------------------`)}
+
+    ${chalk.italic(`I am a WordPress Developer having 7+ years of hands on experience. I ❤️  open-source and have contributed to many open-source projects like GiveWP, Plausible Analytics and more. Now, I believe in learning and hence moving my profile from WordPress developer to more of Full Stack Web Developer.`)}
+
+    ${chalk.bold(`👇👇👇 Connect with me 👇👇👇`)}
+
+    ${chalk.inverse.bold.cyan(` Twitter  `)} https://twitter.com/mehul_gohil0810
+    ${chalk.inverse.bold.bgWhite.blue(` Facebook `)} https://facebook.com/mehulgohilindia
+    ${chalk.inverse.bold.white(` GitHub   `)} https://github.com/mehul0810
+    ${chalk.inverse.bold.green(` Website  `)} https://mehulgohil.com
+    `
+);
